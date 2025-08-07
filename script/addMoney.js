288e39e713e2bbf7211494676c6589204817b374
abdeyrabby404 <abdeyrabby404@gmail.com>
2025-08-08 04:23:23 +0600

    transaction history updated

--- script/addMoney.js
+++ script/addMoney.js
@@ -13,6 +13,13 @@ document.getElementById('add-money-btn')
         const mainAmount = document.getElementById('main-ammount').innerText;
         // console.log(mainAmount);
         const mainAmountFloat = parseFloat(mainAmount);
+
+        if (addAmount < 0){
+            alert("Please enter right amount!!");
+            return;
+        }
+
+
         if(accountNumberr.length === 11){
             if (addAmount) {
             if (pinNumberInt === 1234) {
@@ -20,12 +27,26 @@ document.getElementById('add-money-btn')
                 document.getElementById('main-ammount').innerText = sum;
                 alert(addAmountFloat+ " "+"Money Added Successfully!");
 
+            //     const transactionHistory = document.getElementById("transaction-container");
+            //     const p = document.createElement("p");
+            //     p.innerText = `
+            // Added ${addAmount} from ${accountNumberr} account Succesfully!
+            //     `
+            //     transactionHistory.appendChild(p);
+
                 const transactionHistory = document.getElementById("transaction-container");
-                const p = document.createElement("p");
-                p.innerText = `
-            Added ${addAmount} from ${accountNumberr} account Succesfully!
+
+                const p = document.createElement("div");
+                p.innerHTML = `
+                <div class="border border-gray-300 rounded-lg p-5 bg-slate-100 mb-2">
+                        <h1>Added Money ${addAmount} from ${accountNumberr} Successfully!</h1>
+                </div>
                 `
+                // console.log(div);
                 transactionHistory.appendChild(p);
+
+
+
 
 
 
--- script/cashout.js
+++ script/cashout.js
@@ -19,6 +19,12 @@ document.getElementById('withdraw-money-btn')
     const mainAmount = document.getElementById('main-ammount').innerText;
     const mainAmountFloat = parseFloat(mainAmount);
 
+
+    if (amountWithdraw > mainAmount){
+            alert("You don't have this amount! Please add money first!!");
+            return;
+        }
+
     if(phoneNumber.length === 11){
         if(amountWithdraw){
         if (pinNumberInt === 1234){
@@ -26,12 +32,23 @@ document.getElementById('withdraw-money-btn')
         document.getElementById('main-ammount').innerText = restOfTheBalance;
         alert(amountWithdrawFloat + " " + 'Cashout successfully done!!'); 
 
-        const cashOutTransactionHistory = document.getElementById("transaction-container");
-        const p = document.createElement("p");
-        p.innerText=`
-        Cashout ${amountWithdraw} from ${phoneNumber} successfully!
-        `
-        cashOutTransactionHistory.appendChild(p);
+        // const cashOutTransactionHistory = document.getElementById("transaction-container");
+        // const p = document.createElement("p");
+        // p.innerText=`
+        // Cashout ${amountWithdraw} from ${phoneNumber} successfully!
+        // `
+        // cashOutTransactionHistory.appendChild(p);
+
+        const transactionHistory = document.getElementById("transaction-container");
+
+                const p = document.createElement("div");
+                p.innerHTML = `
+                <div class="border border-gray-300 rounded-lg p-5 bg-slate-100 mb-2">
+                        <h1>Cashout amount ${amountWithdraw} from ${phoneNumber} Successfully!</h1>
+                </div>
+                `
+                // console.log(div);
+                transactionHistory.appendChild(p);
 
 
 
