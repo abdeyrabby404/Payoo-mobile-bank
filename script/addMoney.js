39dc222f039af2d0c69c9219efb0edca24bf3d0b
abdeyrabby404 <abdeyrabby404@gmail.com>
2025-08-08 01:51:31 +0600

    cashout function added

--- script/addMoney.js
+++ script/addMoney.js
@@ -1,33 +1,39 @@
 document.getElementById('add-money-btn')
-.addEventListener('click', function(event){
-    event.preventDefault();
-    
-    const addAmount = document.getElementById('add-ammount').value;
-    const addAmountFloat = parseFloat(addAmount);
-    const pinNumber = document.getElementById('pin-number').value;
-    const pinNumberInt = parseInt(pinNumber);
-    const mainAmount = document.getElementById('main-ammount').innerText;
-    const mainAmountFloat = parseFloat(mainAmount);
+    .addEventListener('click', function (event) {
+        event.preventDefault();
 
+        const accountNumberr = document.getElementById('account-number').value;
 
-    if(addAmount){
-        if(pinNumberInt === 1234){
-        const sum = mainAmountFloat + addAmountFloat;
-        document.getElementById('main-ammount').innerText=sum;
-        alert("Money Added Successfully!");
+        const addAmount = document.getElementById('add-ammount').value;
+        const addAmountFloat = parseFloat(addAmount);
+        const pinNumber = document.getElementById('pin-number').value;
+        const pinNumberInt = parseInt(pinNumber);
+        const mainAmount = document.getElementById('main-ammount').innerText;
+        // console.log(mainAmount);
+        const mainAmountFloat = parseFloat(mainAmount);
+        if(accountNumberr.length === 11){
+            if (addAmount) {
+            if (pinNumberInt === 1234) {
+                const sum = mainAmountFloat + addAmountFloat;
+                document.getElementById('main-ammount').innerText = sum;
+                alert(addAmountFloat+ " "+"Money Added Successfully!");
+            }
+            else {
+                alert(
+                    "Please enter correct PIN number"
+                )
+            }
+        }
+        else {
+            alert("Please enter Amount!");
+        }
+        }
+        else{
+        alert('Enter valid phone Number');
     }
-    else{
-        alert(
-            "Please enter correct PIN number"
-        )
-    }
-    }
-    else{
-        alert("Please enter Amount!");
-    }
-});
+    });
 
 document.getElementById('log-out')
-.addEventListener('click', function(){
-    window.location.href="./index.html"
-})
+    .addEventListener('click', function () {
+        window.location.href = "./index.html"
+    })
---
+++ script/cashout.js
@@ -0,0 +1,47 @@
+document.getElementById('withdraw-money-btn')
+.addEventListener('click', function(event){
+    event.preventDefault();
+
+    const phoneNumber = document.getElementById('agent-number').value;
+    console.log(typeof phoneNumber);
+
+    const amountWithdraw = document.getElementById('cash-amount').value;
+    const amountWithdrawFloat = parseFloat(amountWithdraw);
+
+    // const pinNumber = document.getElementById('pin-number').value;
+    // const pinNumberInt = parseInt(pinNumber);
+
+    const pinNumber = document.getElementById('pin').value;
+    const pinNumberInt = parseInt(pinNumber);
+    console.log(typeof pinNumberInt);
+
+
+    const mainAmount = document.getElementById('main-ammount').innerText;
+    const mainAmountFloat = parseFloat(mainAmount);
+
+    if(phoneNumber.length === 11){
+        if(amountWithdraw){
+        if (pinNumberInt === 1234){
+        const restOfTheBalance = mainAmountFloat - amountWithdrawFloat;
+        document.getElementById('main-ammount').innerText = restOfTheBalance;
+        alert(amountWithdrawFloat + " " + 'Cashout successfully done!!'); 
+        }
+        else{
+            alert('Pin is incorrect');
+        }
+         
+
+    }
+    else{
+        alert('Please enter the amount you want to cashout!');
+    }
+    }
+    else{
+        alert('Enter valid phone Number');
+    }
+
+
+
+
+
+})
